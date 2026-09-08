90bd4d0c9000299e95b011d18f26358047533253
vinsonchen3 <vinsonc0101@gmail.com>
2026-09-07 20:01:17 -0400

    test: added and confirmed parser -> chunker -> embedder pipeline works

--- doc_test.py
+++ doc_test.py
@@ -1,10 +1,19 @@
 from pathlib import Path
-from ingestion.chunker import load_document
+from src.ingestion.chunker import chunk_document
+from src.ingestion.embedder import embed_document
 from src.ingestion.parser import docx_to_sections
 
 path = Path("documents/How-To_ Design an Asymmetric Spinning Weapon.docx")
 
-print(load_document(path))
+chunks = chunk_document(path)
+
+chunks = embed_document(chunks)
+
+for chunk in chunks:
+    print(chunk["source"])
+    print(chunk["heading"])
+    print(len(chunk['embedding']))
+    print()
 # sections = docx_to_sections(path)
 
 # for section in sections:
