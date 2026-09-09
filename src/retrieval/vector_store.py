0534d103cfaaf0cb26c23c6055c93fdbdd6354e0
vinsonchen3 <vinsonc0101@gmail.com>
2026-09-09 16:06:23 -0400

    fix: added the correct embedding function to override the default chromadb

--- src/retrieval/vector_store.py
+++ src/retrieval/vector_store.py
@@ -1,14 +1,20 @@
 from pathlib import Path
 import chromadb
+from chromadb.utils import embedding_functions
 
 project_root = Path(__file__).resolve().parent.parent.parent
 db_path = project_root / "chroma_db"
 
 client = chromadb.PersistentClient(path=str(db_path))
 
+ef = embedding_functions.SentenceTransformerEmbeddingFunction(
+    model_name="all-MiniLM-L6-v2"
+)
+
 collection = client.get_or_create_collection(
     name="combat_robotics",
     metadata={"hnsw:space": "cosine"},
+    embedding_function=ef,
 )
 
 
