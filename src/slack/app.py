96742518ddec5d98ab6a849c6ac1ccfcce1b6a40
vinsonchen3 <vinsonc0101@gmail.com>
2026-09-12 17:06:48 -0400

    feat: added create_app function to handle slack app creation and handler registration

--- src/slack/app.py
+++ src/slack/app.py
@@ -1,27 +1,29 @@
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+from src.ingestion.embedder import model
+from src.retrieval.retriever import Retriever
+from src.retrieval.vector_store import collection
+from src.slack.handlers import create_app_mention_handler
 
 load_dotenv()
-app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
 
-@app.event("app_mention")
-def handle_mention(event, say):
-    question = event["text"]
-
-    say(
-        text=f"You asked: {question}",
-        thread_ts=event["ts"],
+def create_app() -> App:
+    slack_app = App(
+        token=os.environ["SLACK_BOT_TOKEN"],
+        app_token=os.environ["SLACK_APP_TOKEN"],
     )
 
-
-if __name__ == "__main__":
-    handler = SocketModeHandler(
-        app,
-        os.environ["SLACK_APP_TOKEN"],
+    retriever = Retriever(
+        collection=collection,
+        model=model,
+        top_k=4,
     )
 
-    handler.start()
+    handle_app_mention = create_app_mention_handler(retriever=retriever)
+    slack_app.event("app_mention")(handle_app_mention)
+
+    return slack_app
